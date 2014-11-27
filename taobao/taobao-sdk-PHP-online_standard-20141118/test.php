b076083bc2e78b4d83a94402475e2d69b9e41694
Dalocklei <lei.jia08@gmail.com>
2014-11-27 21:12:29 +0800

    add the trade api test

--- taobao/taobao-sdk-PHP-online_standard-20141118/test.php
+++ taobao/taobao-sdk-PHP-online_standard-20141118/test.php
@@ -9,19 +9,26 @@ include "TopSdk.php";
 $c = new TopClient;
 $c->appkey = "1023051402";
 $c->secretKey = "sandbox3c96c15315b1776cbcfd90108";
-$sessionkey= "6100426a11d634f1e707b10c1cd797ebba18eb452f5a0023643570735";   //如沙箱测试帐号sandbox_c_1授权后得到的sessionkey
+$sessionkey= "61029144a9a710e42e5847129db3904d4d52af9cfc7306d3643570735";   //如沙箱测试帐号sandbox_c_1授权后得到的sessionkey
 //实例化具体API对应的Request类
 $req = new UserSellerGetRequest;
 $req->setFields("user_id,nick,sex,seller_credit,type,has_more_pic,item_img_num,item_img_size,prop_img_num,prop_img_size,auto_repost,promoted_type,status");
 //	,alipay_bind,consumer_protection,avatar,liangpin,sign_food_seller_promise,has_shop,is_lightning_consignment,has_sub_stock,is_golden_seller,vip_info,magazine_subscribe,vertical_market,online_gaming");
- 
+
+$req2 = new TradesSoldGetRequest;
+$req2->setFields("seller_nick,buyer_nick,title,type,created,sid,tid,seller_rate,buyer_rate,status,payment");
+$req2->setStartCreated("2014-10-01 00:00:00");
+$req2->setEndCreated("2014-11-28 00:00:00");
+
 //执行API请求并打印结果
 $resp = $c->execute($req,$sessionkey);
+$resp2 = $c->execute($req2, $sessionkey);
 echo "result:";
 echo "<br />";
 print_r($resp);
 echo "<br />";
-
+print_r($resp2);
+echo "<br />";
 //连接数据库
 $db_con = mysql_connect("localhost", "root", "");
 if( !$db_con )
